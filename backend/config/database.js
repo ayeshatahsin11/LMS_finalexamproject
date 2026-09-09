7705135d9aecd55177a94aa34a2359113449c1b5
ayeshatahsin11 <ayeshatahsin1000@gmail.com>
2026-09-09 19:24:41 +0600

    backend initialized

---
+++ backend/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const mainDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB Connection Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = mainDB;
--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,34 @@
-// -------------- main server here --------------------
-
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/database");
+
 const app = express();
 
-app.use(cors());
+// Connect MongoDB
+connectDB();
+
+// Middlewares
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
+// Test route
 app.get("/", (req, res) => {
   res.json({
-    message: "LMS API is running"
+    success: true,
+    message: "LMS API is running",
   });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
